30e728a0c31193ac13c9659bae4b9df8d1f57b2b
Mildred Zaragoza <mildredzaragozav@gmail.com>
2026-03-25 13:44:25 -0600

    Adding Heap exercises.

---
+++ src/main/java/com/mildred/datastructures/exercises/Heap/MaximumElementInAStream.java
@@ -0,0 +1,73 @@
+package com.mildred.datastructures.exercises.Heap;
+
+import com.mildred.datastructures.Heap;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MaximumElementInAStream {
+
+    /**
+     * This method processes a stream of integers and returns the maximum number encountered so far for each input number.
+     * @param nums
+     * @return
+     */
+    public static List<Integer> streamMax(int[] nums) {
+        Heap myHeap = new Heap();
+        List<Integer> myList = new ArrayList<>();
+
+        for (int n : nums) {
+            myHeap.insert(n);
+            myList.add(myHeap.getHeap().get(0));
+        }
+
+        return myList;
+    }
+
+    public static void main(String[] args) {
+        // Test case 1
+        int[] nums1 = {1, 5, 2, 9, 3, 6, 8};
+        System.out.println("Test case 1:");
+        System.out.println("Input: [1, 5, 2, 9, 3, 6, 8]");
+        System.out.println("Expected output: [1, 5, 5, 9, 9, 9, 9]");
+        System.out.println("Actual output: " + streamMax(nums1));
+        System.out.println();
+
+        // Test case 2
+        int[] nums2 = {10, 2, 5, 1, 0, 11, 6};
+        System.out.println("Test case 2:");
+        System.out.println("Input: [10, 2, 5, 1, 0, 11, 6]");
+        System.out.println("Expected output: [10, 10, 10, 10, 10, 11, 11]");
+        System.out.println("Actual output: " + streamMax(nums2));
+        System.out.println();
+
+        // Test case 3
+        int[] nums3 = {3, 3, 3, 3, 3};
+        System.out.println("Test case 3:");
+        System.out.println("Input: [3, 3, 3, 3, 3]");
+        System.out.println("Expected output: [3, 3, 3, 3, 3]");
+        System.out.println("Actual output: " + streamMax(nums3));
+        System.out.println();
+
+        /*
+            EXPECTED OUTPUT:
+            ----------------
+            Test case 1:
+            Input: [1, 5, 2, 9, 3, 6, 8]
+            Expected output: [1, 5, 5, 9, 9, 9, 9]
+            Actual output: [1, 5, 5, 9, 9, 9, 9]
+
+            Test case 2:
+            Input: [10, 2, 5, 1, 0, 11, 6]
+            Expected output: [10, 10, 10, 10, 10, 11, 11]
+            Actual output: [10, 10, 10, 10, 10, 11, 11]
+
+            Test case 3:
+            Input: [3, 3, 3, 3, 3]
+            Expected output: [3, 3, 3, 3, 3]
+            Actual output: [3, 3, 3, 3, 3]
+
+        */
+
+    }
+}
