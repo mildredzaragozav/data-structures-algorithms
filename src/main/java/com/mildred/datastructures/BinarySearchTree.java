6045dfc3d9cb09791d83e620ec75fc3bae36d931
Mildred Zaragoza <mildredzaragozav@gmail.com>
2026-04-03 12:32:07 -0600

    Adding recursive methods to BST, part 1.

--- src/main/java/com/mildred/datastructures/BinarySearchTree.java
+++ src/main/java/com/mildred/datastructures/BinarySearchTree.java
@@ -55,7 +55,44 @@ public class BinarySearchTree {
             }
 
         }
-
         return false;
+    }
+
+    /**
+     * This method is a recursive method designed to search for a specific integer value in the BST.
+     * It checks if the given value exists in the BST and returns a boolean indicating the presence of that value.
+     * @param value
+     * @return
+     */
+    public boolean rContains(int value) {
+        return rContains(root, value);
+    }
+
+    private boolean rContains(Node currentNode, int value) {
+        if (currentNode == null) return false;
+
+        if (currentNode.value == value) return true;
+
+        if (value < currentNode.value) {
+            return rContains(currentNode.left, value);
+        } else {
+            return rContains(currentNode.right, value);
+        }
+    }
+
+    public void rInsert(int value) {
+        if (root == null) root = new Node(value);
+        rInsert(root, value);
+    }
+
+    private Node rInsert(Node currentNode, int value) {
+        if (currentNode == null) return new Node(value);
+
+        if (value < currentNode.value) {
+            currentNode.left = rInsert(currentNode.left, value);
+        } else if (value > currentNode.value) {
+            currentNode.right = rInsert(currentNode.right, value);
+        }
+        return currentNode;
     }
 }
