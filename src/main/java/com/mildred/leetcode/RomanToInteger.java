b94e0e2b3d5a32557ff7a734f7732ed976ba7d3d
Mildred Zaragoza <mildredzaragozav@gmail.com>
2026-04-26 11:28:36 -0600

    Improving solution.

--- src/main/java/com/mildred/leetcode/RomanToInteger.java
+++ src/main/java/com/mildred/leetcode/RomanToInteger.java
@@ -1,9 +1,5 @@
 package com.mildred.leetcode;
 
-import java.util.Map;
-
-import static java.util.Map.entry;
-
 public class RomanToInteger {
     /**
      * Given a roman numeral, convert it to an integer.
@@ -12,37 +8,41 @@ public class RomanToInteger {
      * Complexity: O(N)
      */
     public static int romanToInt(String s) {
-        Map<Character, Integer> values = Map.ofEntries(
-                entry('M', 1000),
-                entry('D', 500),
-                entry('C', 100),
-                entry('L', 50),
-                entry('X', 10),
-                entry('V', 5),
-                entry('I', 1));
-
-        if (s.length() == 1) return values.get(s.charAt(0));
-
         int value = 0;
-        int prev = 0;
+        int prev = getInt(s.charAt(s.length()-1));
 
         for (int i = s.length() - 1; 0 <= i; i--) {
-            int currentValue = values.get(s.charAt(i));
+            int currentValue = getInt(s.charAt(i));
+
             if (currentValue < prev) {
                 value -= currentValue;
             } else {
                 value += currentValue;
             }
+
             prev = currentValue;
         }
         return value;
     }
 
+    private static int getInt(char roman) {
+        return switch (roman) {
+            case 'I' -> 1;
+            case 'V' -> 5;
+            case 'X' -> 10;
+            case 'L' -> 50;
+            case 'C' -> 100;
+            case 'D' -> 500;
+            case 'M' -> 1000;
+            default -> 0;
+        };
+    }
+
     public static void main(String[] args) {
-        System.out.println("IV: " + romanToInt("IV"));
-        System.out.println("XIV: " + romanToInt("XIV"));
-        System.out.println("V: " + romanToInt("V"));
-        System.out.println("VI: " + romanToInt("VI"));
-        System.out.println("MCMXCIV: " + romanToInt("MCMXCIV") + " Expected: 1994");
+        System.out.println("Input: V   Expected: 5  Output: " + romanToInt("V"));
+        System.out.println("Input: IV  Expected: 4  Output: " + romanToInt("IV"));
+        System.out.println("Input: VI  Expected: 6  Output: " + romanToInt("VI"));
+        System.out.println("Input: XIV Expected: 14 Output: " + romanToInt("XIV"));
+        System.out.println("Input: MCMXCIV Expected: 1994 Output: " + romanToInt("MCMXCIV"));
     }
 }
