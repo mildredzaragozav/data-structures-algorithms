1b676dabb7b9a7285ac2772f30e7630bd8e3560a
Mildred Zaragoza <mildredzaragozav@gmail.com>
2026-05-11 10:17:35 -0600

    Updating MergeTwoSortedLists: solution by recursion.

--- src/main/java/com/mildred/leetcode/MergeTwoSortedLists.java
+++ src/main/java/com/mildred/leetcode/MergeTwoSortedLists.java
@@ -23,24 +23,19 @@ public class MergeTwoSortedLists {
      * @return
      */
     public static ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        ListNode temp = new ListNode();
-        ListNode head = temp;
-
-        while (list1 != null && list2 != null) {
+        if (list1 != null && list2 != null) {
             if (list1.val < list2.val) {
-                temp.next = new ListNode(list1.val);
-                temp = temp.next;
-                list1 = list1.next;
+                list1.next = mergeTwoLists(list1.next, list2);
+                return list1;
             } else {
-                temp.next = new ListNode(list2.val);
-                temp = temp.next;
-                list2 = list2.next;
+                list2.next = mergeTwoLists(list1, list2.next);
+                return list2;
             }
         }
 
-        temp.next = (list1 == null ? list2 : list1);
+        if(list1 == null) return list2;
 
-        return head.next;
+        return list1;
     }
 
     private static void printList(ListNode head) {
@@ -67,16 +62,12 @@ public class MergeTwoSortedLists {
 
         printList(mergeTwoLists(n1, a1));
 
-        printList(mergeTwoLists(null,null));
-
         ListNode b1 = new ListNode(0);
         ListNode b2 = new ListNode(0);
         printList(mergeTwoLists(b1,b2));
 
+        printList(mergeTwoLists(null, new ListNode(0)));
 
-        printList(mergeTwoLists(null,b2));
-
-
-
+        printList(mergeTwoLists(null,null));
     }
 }
