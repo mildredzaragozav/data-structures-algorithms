a97ce91b37eb00b2aedaed63521b1fcb05571e55
Mildred Zaragoza <mildredzaragozav@gmail.com>
2026-05-18 14:12:55 -0600

    Adding Move Zeroes exercise: solution using two pointers.

---
+++ src/main/java/com/mildred/leetcode/MoveZeroes.java
@@ -0,0 +1,38 @@
+package com.mildred.leetcode;
+
+import java.util.Arrays;
+
+public class MoveZeroes {
+
+    /**
+     * Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+     * Current complexity for this solution:
+     * Time O(n)
+     * Space O(1)
+     */
+    public static void moveZeroes(int[] nums) {
+        int left = 0;
+        for (int right = 0; right < nums.length; right++) {
+            if (nums[right] != 0) {
+                int temp = nums[left];
+                nums[left] = nums[right];
+                nums[right] = temp;
+                left++;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{0,1,0,3,12};
+        System.out.println("Input: " + Arrays.toString(nums));
+        moveZeroes(nums);
+        System.out.println("Output: " + Arrays.toString(nums));
+        System.out.println("Expected: [1, 3, 12, 0, 0] \n");
+
+        nums = new int[]{0};
+        System.out.println("Input: " + Arrays.toString(nums));
+        moveZeroes(nums);
+        System.out.println("Output: " + Arrays.toString(nums));
+        System.out.println("Expected: [0] \n");
+    }
+}
