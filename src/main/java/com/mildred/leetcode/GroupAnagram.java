296519d84af0f8a7b7cafcb1612191bad655dec0
Mildred Zaragoza <mildredzaragozav@gmail.com>
2026-05-19 17:52:00 -0600

    Adding Group Anagram.

---
+++ src/main/java/com/mildred/leetcode/GroupAnagram.java
@@ -0,0 +1,42 @@
+package com.mildred.leetcode;
+
+import java.util.*;
+
+public class GroupAnagram {
+    /**
+     * Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+     * Note: strs[i] consists of lowercase English letters.
+     * Complexity for current solution:
+     * Time: O(n x mlog(m))
+     * Space: O(1)
+     * @param strs
+     * @return
+     */
+    public static List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> anagrams = new HashMap<>();
+        for (String str : strs) {
+            char[] word = str.toCharArray();
+            Arrays.sort(word);
+            anagrams.computeIfAbsent(Arrays.toString(word), k -> new ArrayList<>()).add(str);
+        }
+
+        return new ArrayList<>(anagrams.values());
+    }
+
+    public static void main(String[] args) {
+        String[] a = new String[]{"eat", "tea", "tan", "ate", "nat", "bat"};
+        System.out.println("Input: " + Arrays.toString(a));
+        System.out.println("Expected: [[bat],[nat,tan],[ate,eat,tea]] (any order)");
+        System.out.println("Output: " + groupAnagrams(a));
+
+        String[] b = new String[]{""};
+        System.out.println("\nInput: " + Arrays.toString(b));
+        System.out.println("Expected: []");
+        System.out.println("Output: " + groupAnagrams(b));
+
+        String[] c = new String[]{"a"};
+        System.out.println("\nInput: " + Arrays.toString(c));
+        System.out.println("Expected: [[a]]");
+        System.out.println("Output: " + groupAnagrams(c));
+    }
+}
